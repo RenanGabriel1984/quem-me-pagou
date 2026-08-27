16c5fcbe07f361d28125640a91eb9ae65270a677
Renan Martins Gabriel <264206799+RenanGabriel1984@users.noreply.github.com>
2026-08-27 16:31:53 +0000

    Before: Vamos adicionar uma melhoria de usabilidade no formulário de criação/edição de Assinatura (modal "No...

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import { vlyPlugin } from "@vly-ai/integrations";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vlyPlugin(), react()],
 })
