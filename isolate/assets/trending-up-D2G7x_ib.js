3f8e2aae5d669df0b8a687257adbbfb658aefe66
Renan Martins Gabriel <264206799+RenanGabriel1984@users.noreply.github.com>
2026-09-02 14:13:14 +0000

    Sync uncommitted changes from freebuff.com

---
+++ isolate/assets/trending-up-D2G7x_ib.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-UgRO6Q8z.js";const n=[["path",{d:"M12 15V3",key:"m9g1x1"}],["path",{d:"M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4",key:"ih7n3h"}],["path",{d:"m7 10 5 5 5-5",key:"brsn70"}]],d=o("download",n);const t=[["path",{d:"M16 7h6v6",key:"box55l"}],["path",{d:"m22 7-8.5 8.5-5-5L2 17",key:"1t1m79"}]],e=o("trending-up",t);export{d as D,e as T};
